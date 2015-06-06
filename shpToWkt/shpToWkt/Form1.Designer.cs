ba600f9565c5c8544402f974ad487ae14c6f9c13
Ruijie128 <763735677@qq.com>
2015-06-06 21:23:29 +0800

    convert shpfile to wkt

---
+++ shpToWkt/shpToWkt/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace shpToWkt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapBox1 = new SharpMap.Forms.MapBox();
+            this.openShpFile = new System.Windows.Forms.Button();
+            this.transform = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // mapBox1
+            // 
+            this.mapBox1.ActiveTool = SharpMap.Forms.MapBox.Tools.None;
+            this.mapBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mapBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.mapBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.mapBox1.FineZoomFactor = 10D;
+            this.mapBox1.Location = new System.Drawing.Point(133, 0);
+            this.mapBox1.MapQueryMode = SharpMap.Forms.MapBox.MapQueryType.LayerByIndex;
+            this.mapBox1.Name = "mapBox1";
+            this.mapBox1.QueryGrowFactor = 5F;
+            this.mapBox1.QueryLayerIndex = 0;
+            this.mapBox1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.mapBox1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.mapBox1.ShowProgressUpdate = false;
+            this.mapBox1.Size = new System.Drawing.Size(581, 546);
+            this.mapBox1.TabIndex = 0;
+            this.mapBox1.Text = "mapBox1";
+            this.mapBox1.WheelZoomMagnitude = -2D;
+            // 
+            // openShpFile
+            // 
+            this.openShpFile.Location = new System.Drawing.Point(12, 31);
+            this.openShpFile.Name = "openShpFile";
+            this.openShpFile.Size = new System.Drawing.Size(115, 23);
+            this.openShpFile.TabIndex = 1;
+            this.openShpFile.Text = "打开shp文件";
+            this.openShpFile.UseVisualStyleBackColor = true;
+            this.openShpFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // transform
+            // 
+            this.transform.Location = new System.Drawing.Point(12, 102);
+            this.transform.Name = "transform";
+            this.transform.Size = new System.Drawing.Size(115, 23);
+            this.transform.TabIndex = 2;
+            this.transform.Text = "转为为WKT格式";
+            this.transform.UseVisualStyleBackColor = true;
+            this.transform.Click += new System.EventHandler(this.transform_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 400);
+            this.Controls.Add(this.transform);
+            this.Controls.Add(this.openShpFile);
+            this.Controls.Add(this.mapBox1);
+            this.Name = "Form1";
+            this.Text = "shp文件转换";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpMap.Forms.MapBox mapBox1;
+        private System.Windows.Forms.Button openShpFile;
+        private System.Windows.Forms.Button transform;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ shpToWkt/shpToWkt/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpMap;
+using SharpMap.Forms;
+using SharpMap.Layers;
+using SharpMap.Data;
+using GeoAPI.Geometries;
+using SharpMap.Utilities;
+using SharpMap.Rendering;
+using GeoAPI;
+using System.IO;
+
+namespace shpToWkt
+{
+    public partial class Form1 : Form
+    {
+        public static string file;
+        public static SharpMap.Layers.VectorLayer vlay = new SharpMap.Layers.VectorLayer("newLayer");
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+          /*  if (ds == null) return;
+            System.Data.DataTable dt = new DataTable();
+            System.Data.DataColumn dc = new DataColumn("名字", typeof(System.String));
+            dt.Columns.Add(dc);
+            foreach (SharpMap.Data.FeatureDataRow row in ds.Tables[0])
+            {
+                if (row["NAME"].ToString().Contains(this.txtQuery.Text.Trim()))
+                {
+                    System.Data.DataRow r = dt.NewRow();
+                    r[0] = row["NAME"].ToString();
+                    dt.Rows.Add(r);
+                }
+            }
+            this.gvResults.DataSource = dt;
+            this.gvResults.DataBind();
+            //重新生成地图
+            this.GenerateMap();*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "(*.shp*)|*.shp*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                
+                file = openFileDialog1.FileName;}
+           // SharpMap.Layers.VectorLayer vlay = new SharpMap.Layers.VectorLayer("newLayer");
+            if (file == null)
+            {
+                MessageBox.Show("请选择shp文件");
+                return;
+            }
+            vlay.DataSource = new SharpMap.Data.Providers.ShapeFile(file, true);
+           // vlay.Render(new Graphics(),mapBox1.Map);
+            mapBox1.Map.Layers.Add(vlay);
+            mapBox1.Map.ZoomToExtents();
+
+            mapBox1.Refresh();
+
+            
+        }
+
+        private void transform_Click(object sender, EventArgs e)
+        {
+
+            if (vlay == null) return;
+            if (vlay.DataSource == null)
+                MessageBox.Show("请先载入shp文件");
+            else
+            {
+                if (!vlay.DataSource.IsOpen) vlay.DataSource.Open();
+                SharpMap.Data.Providers.ShapeFile shapefile = (SharpMap.Data.Providers.ShapeFile)vlay.DataSource;
+                int geoCount = shapefile.GetFeatureCount();
+                string writefile = null;
+                saveFileDialog1.Filter = "(*.txt*)|*.txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    writefile = saveFileDialog1.FileName;
+                }
+                if (writefile == null)
+                {
+                    MessageBox.Show("输出文件不能为空，请正确选择");
+                    return;
+                }
+                FileStream fs = new FileStream(writefile, FileMode.Append);
+                StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+                for (int i = 0; i < geoCount; i++)
+                {
+
+                    GeoAPI.Geometries.IGeometry geo = shapefile.GetGeometryByID(Convert.ToUInt32(i));
+                    sw.Write(geo + "\n");
+                }
+
+                //   Console.WriteLine(geo);
+                vlay.DataSource.Close();
+
+
+
+                sw.Close();
+                fs.Close();
+                MessageBox.Show("文件已转换完毕！");
+            }
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
